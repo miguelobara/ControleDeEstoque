9e5e6fb3d45a9162e7e38ee6b92df5350513f96e
miguelobara <miguel.obara@aluno.ifsp.edu.br>
2025-09-09 10:14:36 -0300

    aaaa

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -30,20 +30,30 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Cpf = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.senhaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.usuarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.cJ3027511PR2DataSet = new ControleDeEstoque.CJ3027511PR2DataSet();
             this.usuarioTableAdapter = new ControleDeEstoque.CJ3027511PR2DataSetTableAdapters.UsuarioTableAdapter();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.tbxNome_Prod = new System.Windows.Forms.TextBox();
+            this.tbxCategoria = new System.Windows.Forms.TextBox();
+            this.tbxValidade = new System.Windows.Forms.TextBox();
+            this.tbxDescricao = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.Cpf = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbxNome_Tipo = new System.Windows.Forms.TextBox();
+            this.tbxUnidade_Medida = new System.Windows.Forms.TextBox();
+            this.btnDeletarTipo = new System.Windows.Forms.Button();
+            this.btnRenomearTip = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmbNome_Tipo = new System.Windows.Forms.ComboBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.usuarioBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cJ3027511PR2DataSet)).BeginInit();
@@ -55,13 +65,33 @@
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Cpf});
+            this.Cpf,
+            this.emailDataGridViewTextBoxColumn,
+            this.senhaDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.usuarioBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(13, 298);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(727, 169);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick);
+            // 
+            // Cpf
+            // 
+            this.Cpf.DataPropertyName = "Senha";
+            this.Cpf.HeaderText = "Cpf";
+            this.Cpf.Name = "Cpf";
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            // 
+            // senhaDataGridViewTextBoxColumn
+            // 
+            this.senhaDataGridViewTextBoxColumn.DataPropertyName = "Senha";
+            this.senhaDataGridViewTextBoxColumn.HeaderText = "Senha";
+            this.senhaDataGridViewTextBoxColumn.Name = "senhaDataGridViewTextBoxColumn";
             // 
             // usuarioBindingSource
             // 
@@ -95,67 +125,60 @@
             this.button1.Text = "procurar";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // tbxNome_Prod
             // 
-            this.textBox2.Location = new System.Drawing.Point(13, 65);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "Nome do Produto";
-            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            this.tbxNome_Prod.Location = new System.Drawing.Point(13, 50);
+            this.tbxNome_Prod.Name = "tbxNome_Prod";
+            this.tbxNome_Prod.Size = new System.Drawing.Size(100, 20);
+            this.tbxNome_Prod.TabIndex = 3;
+            this.tbxNome_Prod.Text = "Nome do Produto";
+            this.tbxNome_Prod.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbxNome_Prod.Click += new System.EventHandler(this.textBox2_Click);
             // 
-            // textBox3
+            // tbxCategoria
             // 
-            this.textBox3.Location = new System.Drawing.Point(13, 92);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 4;
-            this.textBox3.Text = "Categoria";
-            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            this.tbxCategoria.Location = new System.Drawing.Point(13, 76);
+            this.tbxCategoria.Name = "tbxCategoria";
+            this.tbxCategoria.Size = new System.Drawing.Size(100, 20);
+            this.tbxCategoria.TabIndex = 4;
+            this.tbxCategoria.Text = "Categoria";
+            this.tbxCategoria.Click += new System.EventHandler(this.textBox3_Click);
             // 
-            // textBox4
+            // tbxValidade
             // 
-            this.textBox4.Location = new System.Drawing.Point(13, 119);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 5;
-            this.textBox4.Text = "Validade";
-            this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
+            this.tbxValidade.Location = new System.Drawing.Point(13, 102);
+            this.tbxValidade.Name = "tbxValidade";
+            this.tbxValidade.Size = new System.Drawing.Size(100, 20);
+            this.tbxValidade.TabIndex = 5;
+            this.tbxValidade.Text = "Validade";
+            this.tbxValidade.Click += new System.EventHandler(this.textBox4_Click);
             // 
-            // textBox5
+            // tbxDescricao
             // 
-            this.textBox5.Location = new System.Drawing.Point(12, 171);
-            this.textBox5.MaxLength = 256;
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(101, 20);
-            this.textBox5.TabIndex = 6;
-            this.textBox5.Text = "Descricao";
-            this.textBox5.Click += new System.EventHandler(this.textBox5_Click);
-            this.textBox5.Enter += new System.EventHandler(this.textBox5_Enter);
-            this.textBox5.Leave += new System.EventHandler(this.textBox5_Leave);
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(13, 145);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 7;
-            this.textBox6.Text = "Tipo";
-            this.textBox6.Click += new System.EventHandler(this.textBox6_Click);
+            this.tbxDescricao.Location = new System.Drawing.Point(11, 180);
+            this.tbxDescricao.MaxLength = 256;
+            this.tbxDescricao.Multiline = true;
+            this.tbxDescricao.Name = "tbxDescricao";
+            this.tbxDescricao.Size = new System.Drawing.Size(101, 20);
+            this.tbxDescricao.TabIndex = 6;
+            this.tbxDescricao.Text = "Descricao";
+            this.tbxDescricao.Click += new System.EventHandler(this.textBox5_Click);
+            this.tbxDescricao.Enter += new System.EventHandler(this.textBox5_Enter);
+            this.tbxDescricao.Leave += new System.EventHandler(this.textBox5_Leave);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(144, 119);
+            this.button2.Location = new System.Drawing.Point(157, 92);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 8;
             this.button2.Text = "adicionar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(241, 119);
+            this.button3.Location = new System.Drawing.Point(157, 116);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 9;
@@ -164,18 +187,95 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(342, 119);
+            this.button4.Location = new System.Drawing.Point(157, 142);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 10;
             this.button4.Text = "deletar";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // Cpf
+            // tbxNome_Tipo
             // 
-            this.Cpf.DataPropertyName = "Senha";
-            this.Cpf.HeaderText = "Cpf";
-            this.Cpf.Name = "Cpf";
+            this.tbxNome_Tipo.Location = new System.Drawing.Point(572, 47);
+            this.tbxNome_Tipo.Name = "tbxNome_Tipo";
+            this.tbxNome_Tipo.Size = new System.Drawing.Size(100, 20);
+            this.tbxNome_Tipo.TabIndex = 12;
+            this.tbxNome_Tipo.Text = "Nome";
+            // 
+            // tbxUnidade_Medida
+            // 
+            this.tbxUnidade_Medida.Location = new System.Drawing.Point(572, 95);
+            this.tbxUnidade_Medida.Name = "tbxUnidade_Medida";
+            this.tbxUnidade_Medida.Size = new System.Drawing.Size(100, 20);
+            this.tbxUnidade_Medida.TabIndex = 13;
+            this.tbxUnidade_Medida.Text = "Unidade De Medida";
+            // 
+            // btnDeletarTipo
+            // 
+            this.btnDeletarTipo.Location = new System.Drawing.Point(470, 102);
+            this.btnDeletarTipo.Name = "btnDeletarTipo";
+            this.btnDeletarTipo.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletarTipo.TabIndex = 15;
+            this.btnDeletarTipo.Text = "DeletarTipo";
+            this.btnDeletarTipo.UseVisualStyleBackColor = true;
+            this.btnDeletarTipo.Click += new System.EventHandler(this.btnDeletarTipo_Click);
+            // 
+            // btnRenomearTip
+            // 
+            this.btnRenomearTip.Location = new System.Drawing.Point(470, 74);
+            this.btnRenomearTip.Name = "btnRenomearTip";
+            this.btnRenomearTip.Size = new System.Drawing.Size(75, 23);
+            this.btnRenomearTip.TabIndex = 16;
+            this.btnRenomearTip.Text = "RenomearTip";
+            this.btnRenomearTip.UseVisualStyleBackColor = true;
+            this.btnRenomearTip.Click += new System.EventHandler(this.btnRenomearTip_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(11, 154);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // cmbNome_Tipo
+            // 
+            this.cmbNome_Tipo.DataSource = this.usuarioBindingSource;
+            this.cmbNome_Tipo.FormattingEnabled = true;
+            this.cmbNome_Tipo.Location = new System.Drawing.Point(11, 128);
+            this.cmbNome_Tipo.Name = "cmbNome_Tipo";
+            this.cmbNome_Tipo.Size = new System.Drawing.Size(121, 21);
+            this.cmbNome_Tipo.TabIndex = 18;
+            this.cmbNome_Tipo.SelectedIndexChanged += new System.EventHandler(this.cmbNome_Tipo_SelectedIndexChanged);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(470, 48);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 19;
+            this.btnAdicionar.Text = "AdicionarTipo";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(572, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "coloque aqui a unidade de medida";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(551, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "coloque se e item de linpesa, bebida, etc";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form4
             // 
@@ -184,14 +284,22 @@
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(763, 479);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.cmbNome_Tipo);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnRenomearTip);
+            this.Controls.Add(this.btnDeletarTipo);
+            this.Controls.Add(this.tbxUnidade_Medida);
+            this.Controls.Add(this.tbxNome_Tipo);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tbxDescricao);
+            this.Controls.Add(this.tbxValidade);
+            this.Controls.Add(this.tbxCategoria);
+            this.Controls.Add(this.tbxNome_Prod);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
@@ -214,14 +322,25 @@
         private CJ3027511PR2DataSetTableAdapters.UsuarioTableAdapter usuarioTableAdapter;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox tbxNome_Prod;
+        private System.Windows.Forms.TextBox tbxCategoria;
+        private System.Windows.Forms.TextBox tbxValidade;
+        private System.Windows.Forms.TextBox tbxDescricao;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Cpf;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn senhaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnAdicionarTipo;
+        private System.Windows.Forms.TextBox tbxNome_Tipo;
+        private System.Windows.Forms.TextBox tbxUnidade_Medida;
+        private System.Windows.Forms.Button btnDeletarTipo;
+        private System.Windows.Forms.Button btnRenomearTip;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmbNome_Tipo;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,12 +20,12 @@ namespace ControleDeEstoque
 
         private void Form4_Load(object sender, EventArgs e)
         {
-           
+
             this.usuarioTableAdapter.Fill(this.cJ3027511PR2DataSet.Usuario);
 
         }
 
-       
+
 
         private void textBox1_Click(object sender, EventArgs e)
         {
@@ -33,37 +34,37 @@ namespace ControleDeEstoque
 
         private void textBox2_Click(object sender, EventArgs e)
         {
-            textBox2.Clear();
+            tbxNome_Prod.Clear();
         }
 
         private void textBox3_Click(object sender, EventArgs e)
         {
-            textBox3.Clear();
+            tbxCategoria.Clear();
         }
 
         private void textBox4_Click(object sender, EventArgs e)
         {
-            textBox4.Clear();
+            tbxValidade.Clear();
         }
 
         private void textBox5_Click(object sender, EventArgs e)
         {
-            textBox5.Clear();
+            tbxDescricao.Clear();
         }
 
         private void textBox5_Enter(object sender, EventArgs e)
         {
-            this.textBox5.Size = new System.Drawing.Size(728, 106);
+            this.tbxDescricao.Size = new System.Drawing.Size(728, 106);
         }
 
         private void textBox5_Leave(object sender, EventArgs e)
         {
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.tbxDescricao.Size = new System.Drawing.Size(100, 22);
         }
 
         private void textBox6_Click(object sender, EventArgs e)
         {
-            textBox6.Clear();
+            
         }
 
         private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -71,13 +72,113 @@ namespace ControleDeEstoque
 
         }
 
-       
+
 
         private void textBox7_Click(object sender, EventArgs e)
         {
 
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string conexao = "Data Source=sqlexpress;Initial Catalog=CJ3027511PR2;User ID=aluno;Password=aluno;";
+
+            // Coleta os dados dos campos do formulário
+            string nome = tbxNome_Prod.Text;
+            string categoria = tbxCategoria.Text;
+            DateTime validade = DateTime.Parse(tbxValidade.Text); // Certifique-se de que o formato está correto
+            string descricao = tbxDescricao.Text;
+
+            using (SqlConnection conn = new SqlConnection(conexao))
+            {
+                string sql = @"INSERT INTO Produto 
+                       (Nome_Prod, Categoria, Validade, Id_Tipo, Descricao) 
+                       VALUES 
+                       (@Nome, @Categoria, @Validade, @Tipo, @Descricao)";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Nome", nome);
+                cmd.Parameters.AddWithValue("@Categoria", categoria);
+                cmd.Parameters.AddWithValue("@Validade", validade);
+                cmd.Parameters.AddWithValue("@Descricao", descricao);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Produto adicionado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao adicionar produto: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            string conexao = "Data Source=sqlexpress;Initial Catalog=CJ3027511PR2;User ID=aluno;Password=aluno;";
+            string nome = tbxNome_Tipo.Text;
+            string unidade = tbxUnidade_Medida.Text;
+
+            using (SqlConnection conn = new SqlConnection(conexao))
+            {
+                string sql = "INSERT INTO Tipo (Nome, Unidade_Medida) VALUES (@Nome, @Unidade)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Nome", nome);
+                cmd.Parameters.AddWithValue("@Unidade", unidade);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Tipo adicionado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao adicionar tipo: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnRenomearTip_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void btnDeletarTipo_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void cmbNome_Tipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string conexao = "Data Source=sqlexpress;Initial Catalog=CJ3027511PR2;User ID=aluno;Password=aluno;";
+
+            using (SqlConnection conn = new SqlConnection(conexao))
+            {
+                string sql = "SELECT Nome FROM Tipo";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    cmbNome_Tipo.Items.Add(reader["Nome"].ToString());
+                }
+            }
+        }
+
         
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
